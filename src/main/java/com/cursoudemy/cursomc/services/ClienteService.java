7e55fbc27f7419c7e624cc0bd218fb7037126486
Leonardo Christoni <leonardo.christoni@gmail.com>
2020-01-24 17:48:35 -0300

    Ajustando a delecao de Cliente

--- src/main/java/com/cursoudemy/cursomc/services/ClienteService.java
+++ src/main/java/com/cursoudemy/cursomc/services/ClienteService.java
@@ -57,7 +57,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		}catch(DataIntegrityViolationException e) {
-			throw new DataIntegratyException("Não é possível excluir porque há entidades relacionadas");
+			throw new DataIntegratyException("Não é possível excluir porque há pedidos relacionadas");
 		}
 	}
 	
